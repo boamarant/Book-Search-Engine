5632e6b695521b1a9efc9c9e34a7c42328846d5a
BoAmarant <bobamarant8@gmail.com>
2024-09-17 22:25:39 -0400

    Resolving errors and trying to make things work correctly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,38 @@
-import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
-import LoginForm from './components/LoginForm';
-import SignupForm from './components/SignupForm';
+import Navbar from './components/Navbar';
 
-const client = new ApolloClient({
+// Setup HTTP link for GraphQL
+const httpLink = createHttpLink({
   uri: '/graphql',
+});
+
+// Attach the token to every request
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <div>
-          <Routes>
-            <Route path="/" element={<SearchBooks />} />
-            <Route path="/saved" element={<SavedBooks />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/signup" element={<SignupForm />} />
-          </Routes>
-        </div>
-      </Router>
+      <Navbar />
+      <SearchBooks />
+      <SavedBooks />
     </ApolloProvider>
   );
 }
