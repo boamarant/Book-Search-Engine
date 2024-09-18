1b34f8041dc64fca50a6f6f12b7b859ceff11449
BoAmarant <bobamarant8@gmail.com>
2024-09-17 21:39:27 -0400

    installed dependencies and made some adjustments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
 import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
-import App from './App';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
 
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
 });
 
-function MyApp() {
+function App() {
   return (
     <ApolloProvider client={client}>
-      <App />
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<SearchBooks />} />
+            <Route path="/saved" element={<SavedBooks />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/signup" element={<SignupForm />} />
+          </Routes>
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
 
-export default MyApp;
+export default App;
